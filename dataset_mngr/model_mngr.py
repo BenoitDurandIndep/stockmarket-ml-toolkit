f15b56b9718ad78c2d4d4759ee7633bda72e82cd
Benoit Durand <benoit.durand.indep@gmail.com>
2022-12-04 18:00:56 +0700

    start model_mngr add splt_df_x_y

---
+++ dataset_mngr/model_mngr.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+def report_best_scores(results, n_top=3):
+    """_summary_
+
+    Args:
+        results (_type_): _description_
+        n_top (int, optional): _description_. Defaults to 3.
+    """
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
+
+
+if __name__ == "__main__":
+    print("Hello world")
